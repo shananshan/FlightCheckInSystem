97a2dc0fbf5db39c9b2209a96be1fe80a95da859
Yukiyt778 <135519587+Yukiyt778@users.noreply.github.com>
2024-02-28 00:49:46 +0000

    Update CheckInGUI.java
    1. have added mismatch message in card 1
    2. have changed the text of switchbutton according to the value of fee
    3. have added card 4 for transaction information
    4. have changed fee to global variable(maybe change back later)
    5. have changed the logic of the flow
    6. have made the class extend JFrame
    7. haven't completed the GUI and function(boolean status of checked-in passengers) of card 4

--- Flight/src/CheckInGUI.java
+++ Flight/src/CheckInGUI.java
@@ -2,16 +2,24 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Objects;
 
-public class CheckInGUI {
+public class CheckInGUI extends JFrame{
+	double fee;
     public CheckInGUI() throws IOException {
         FlightCheckInSystem fcs = new FlightCheckInSystem();
-        fcs.readPassengers("Passenger Bookings.csv");
-        fcs.readFlights("Flight Detail.csv");
+        try {
+            fcs.readPassengers("Passenger Bookings.csv");
+            fcs.readFlights("Flight Detail.csv");
+         }catch(FileNotFoundException e){
+          e.printStackTrace();
+         }catch(IOException e) {
+          e.printStackTrace();
+         }
 
-        JFrame frame = new JFrame("CardLayout Example");
+        JFrame frame = new JFrame("Flight Check-In System");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 300);
 
@@ -88,11 +96,21 @@ public class CheckInGUI {
             chek.add(res[i]);
         }
         card3.add(chek);
+        
+        // Card4: transaction panel
+        JPanel card4 = new JPanel();
+        card4.setName("Card4");
+        JPanel pay = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        pay.setLayout(new GridLayout(2, 2));
+
 
         cardPanel.add(card1, "Card 1");
         cardPanel.add(card2, "Card 2");
         cardPanel.add(card3, "Card 3");
-
+        cardPanel.add(card4, "Card 4");
+        
+        
+        
         JButton switchButton = new JButton("Submit");
         switchButton.addActionListener(new ActionListener() {
             @Override
@@ -105,6 +123,7 @@ public class CheckInGUI {
                     if(!fcs.checkIn(textField1.getText(), textField2.getText())){
                         tname.setText("the input is invalid! Please input again!");
                         System.out.println("failed");
+                        JOptionPane.showMessageDialog(frame, "Information Mismatch");
                         return;
                     }
                     else {
@@ -117,7 +136,7 @@ public class CheckInGUI {
                         switchButton.setText("Confirm");
                     }
                 }
-                else if(Objects.equals(currentCard, "Card2")) {
+                if(Objects.equals(currentCard, "Card2")) {
                     Passenger p = fcs.getPassenger(textField1.getText(), textField2.getText());
                     Flight f = fcs.getFlight(p.getFlightCode());
                     double w, h, l, wi;
@@ -125,23 +144,83 @@ public class CheckInGUI {
                     h = Double.valueOf(filedBags[1].getText());
                     l = Double.valueOf(filedBags[2].getText());
                     wi = Double.valueOf(filedBags[3].getText());
-                    double fee = f.calculateFee(w,h,l,wi);
+                    fee = f.calculateFee(w,h,l,wi);
                     labels[0].setText(name2[0] + ": " + p.name);
                     labels[1].setText(name2[1] + ": " + p.flightCode);
                     labels[2].setText(name2[2] + ": " + p.bookingRefCode);
                     labels[3].setText(name2[3] + ": " + Double.toString(fee));
-                    switchButton.setText("Exit");
+                    if(fee == 0) {
+                    	switchButton.setText("Exit");
+//                    	cardLayout.show(card3, "Card 3");
+                    }
+                    else {
+                        card4.add(new JLabel("Please pay your excess baggage fee:" + fee));
+                        switchButton.setText("Pay your excess baggage fee");
+//                        cardLayout.show(card4, "Card 4");
+                    }
                 }
-                else if(Objects.equals(currentCard, "Card3")) {
+                if(Objects.equals(currentCard, "Card3")) {
+                	if(e.getActionCommand().equals("Exit")) {
+                		System.exit(0);
+                	}
+//                    card3.remove(switchButton);
+//                    card3.validate();
+//                    card3.repaint();
+//                	 if(fee == 0) {
+
+//                           	if(e.getActionCommand().equals("Exit")) {
+//                                
+                                 
+//                	        card3.add(new JButton("Exit") {
+//                	            public void actionPerformed(ActionEvent e) {
+//                	                cardLayout.show(card1, "Card 1");
+//                	                cardLayout.show(card2, "Card 2");
+//                	            }
+//                	        });
+//                	        
+//                         switchButton.setText("Exit");
+//                             @Override
+//                             public void actionPerformed(ActionEvent e) {
+//                             	if(e.getActionCommand().equals("Exit")) {
+//                                   // go back to card 1 and clear the information
+//                             		cardLayout.show(card1, "Card 1");
+//                	                cardLayout.show(card2, "Card 2");
+//                                   
+//                             	}
+//                             }
+//                	 }
+//                     else {
+                             		//go to card 4 
+
+//                         card4.add(new JLabel("Please pay your excess baggage fee:" + fee));
+//                         switchButton.setText("Pay your excess baggage fee");
+//                         cardLayout.show(card4, "Card 4");
+
+//                     }
+
 
                 }
-
-
-
+                if(Objects.equals(currentCard, "Card4")) {
+//                	if(e.getActionCommand().equals("transaction complete")) {
+//                		System.exit(0);
+//                	}
+                    card4.remove(switchButton);
+                    JButton successButton = new JButton("transaction complete");
+                    JButton failureButton = new JButton("transaction incomplete");
+                    card4.add(successButton);
+                    card4.add(failureButton);
+                    card4.validate();
+                    card4.repaint();
+//                	else {
+//                		
+//                	}
+                }
                 // deal with the right flow
                 cardLayout.next(cardPanel);
                 currentCard = getCurrentCardName(cardPanel);
                 frame.setTitle(currentCard);
+                
+            
             }
         });
 
