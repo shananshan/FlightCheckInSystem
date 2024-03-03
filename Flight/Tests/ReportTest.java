dfaa0680f450f1a8b654119004c7ecb77e748342
shanshan <576604497@qq.com>
2024-03-03 16:28:05 +0000

    add some datas

--- Flight/Tests/ReportTest.java
+++ Flight/Tests/ReportTest.java
@@ -3,10 +3,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class ReportTest {
     @Test
-	public void testReport() {
-        ArrayList<CheckInPassenger> checkInPas = new ArrayList<CheckInPassenger>();
-        CheckInPassenger pas = new CheckInPassenger("PA-5723","ZOE","PA-108489",230,30,200);
-        checkInPas.add(pas);
+    public void testReport() {
+        ArrayList<CheckInPassenger> checkInPas = new ArrayList<>();
+        CheckInPassenger pas1 = new CheckInPassenger("PA-5723","ZOE","PA-108489",230,30,100);
+        CheckInPassenger pas2 = new CheckInPassenger("PA-5723","Boy","PA-846582",400,30,100);
+        CheckInPassenger pas3 = new CheckInPassenger("BC-2859","Cat","BC-846582",400,30,100);
+        checkInPas.add(pas1);
+        checkInPas.add(pas2);
+        checkInPas.add(pas3);
         Report report = new Report(checkInPas);
         String filePath = "test_report.txt";
 
