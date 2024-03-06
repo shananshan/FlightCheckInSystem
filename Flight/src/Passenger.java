f31a0b20524409a4892a9ce1ed62d422971b2faf
luxuryLIu <146202585+luxuryLIu@users.noreply.github.com>
2024-03-06 22:02:35 +0000

    Update Passenger.java

--- Flight/src/Passenger.java
+++ Flight/src/Passenger.java
@@ -1,43 +1,53 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Objects;
 
 public class Passenger {
+    // Passenger booking reference code
     String bookingRefCode;
+    // Full name of the passenger
     String name;
+    // Code of the flight the passenger is booked on
     String flightCode;
+    // Indicates if the passenger has passed some check
     boolean passCheckOrNot;
-
+    // Last name of the passenger
     String lastName;
 
-    Passenger(){}
-    Passenger(String [] args){
+    // Default constructor
+    Passenger() {}
+
+    // Constructor with details
+    Passenger(String[] args) {
         bookingRefCode = args[0];
         name = args[1] + " " + args[2];
         lastName = args[2];
         flightCode = args[3];
-        passCheckOrNot = Objects.equals(args[4], "TRUE");//数组第四个元素是否等于TRUE
+        passCheckOrNot = Objects.equals(args[4], "TRUE");
     }
 
+    // Returns passenger's name and booking code (placeholder for future use)
     String[] readNameAndCode() {
         return new String[0];
     }
 
-    boolean findPass(String pass, String ch){
+    // Checks if passenger meets specified criteria (placeholder method)
+    boolean findPass(String pass, String ch) {
         return passCheckOrNot;
     }
 
+    // Returns the flight code
     String getFlightCode() {
         return flightCode;
     }
 
-    public String toString(){
-        return bookingRefCode + "," + name + "," + flightCode + "," + (passCheckOrNot?"True":"False");
+    // Returns a string representation of the passenger
+    @Override
+    public String toString() {
+        return bookingRefCode + "," + name + "," + flightCode + "," + (passCheckOrNot ? "True" : "False");
     }
 
-    public boolean checkOne(String lastname, String flightcode){
+    // Checks if the passenger's last name and flight code match given values
+    public boolean checkOne(String lastname, String flightcode) {
         return Objects.equals(lastname, lastName) && Objects.equals(flightcode, bookingRefCode);
     }
 }
+
