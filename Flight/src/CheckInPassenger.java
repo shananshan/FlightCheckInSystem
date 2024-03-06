24fa7c6d6801405865fd21dce7ab6ef908f72e40
shanshan <576604497@qq.com>
2024-03-06 10:52:29 +0000

    add comments

--- Flight/src/CheckInPassenger.java
+++ Flight/src/CheckInPassenger.java
@@ -1,14 +1,14 @@
 import java.util.ArrayList;
 
 public class CheckInPassenger {
-    // flight code, name, size, weight, fee
+    // flight code, name, booking code, size, weight, fee
 
-    private String flightCode;
+    private String flightCode;  // To tag passengers on the same flight
     private String name;
-    private String bookingCode;
-    private double size;
-    private double weight;
-    private double fee;
+    private String bookingCode; // To check for duplicated travelers
+    private double size;        // To sum the size of all passengers
+    private double weight;      // sum the weight
+    private double fee;         // sum the fee
 
     public CheckInPassenger(String flightCode, String name,String bookingCode, double size, double weight, double fee) {
         this.flightCode = flightCode;
@@ -20,6 +20,7 @@ public class CheckInPassenger {
     }
 
     // Getter and setter methods for each attribute
+    // To facilitate report class to call these variables
 
     public String getFlightCode() { return flightCode; }
 
@@ -66,6 +67,7 @@ public class CheckInPassenger {
     }
 
     public static boolean duplicatePassenger(String newFlightCode, ArrayList<CheckInPassenger> checkinpassengerList){
+        // check if the passenger has checked
         boolean duplicatePassenger = false;
         for (CheckInPassenger passenger : checkinpassengerList){
             if (passenger.getBookingCode().equals(newFlightCode)){
